c87ba92ae3b807bb6043cdc82cfb0e46ec605078
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2024-04-19 10:40:53 +0530

    adding on oops

--- OOPs.java
+++ OOPs.java
@@ -1,29 +1,34 @@
-public class OOPs{
-    public static void main(String[] args){
-        Pen p1 = new Pen();
-        p1.SetColor("Blue");
-        System.out.println(p1.color);
+public class OOPs {
 
-
-    }
-
+  public static void main(String[] args) {
+    Pen p1 = new Pen();
+    p1.SetColor("Blue");
+    System.out.println(p1.color);
+    p1.SetTip(5);
+    System.out.println(p1.tip);
+    p1.SetColor("yellow");
+    System.out.println(p1.color);
+  }
 }
-class Pen{
-    //prop + function
-    String color;
-    int tip;
 
-    void SetColor(String newColor){
-        color = newColor;
-    }
-    
+class Pen {
+
+  //prop + function
+  String color;
+  int tip;
+
+  void SetColor(String newColor) {
+    color = newColor;
+  }
+
+  void SetTip(int newTip) {
+    tip = newTip;
+  }
 }
-    class Student{
-        String name;
-        int age;
-        float percentage;
 
-    }
+class Student {
 
-    
-
+  String name;
+  int age;
+  float percentage;
+}
